448dd235fcbfe69a300c3825235be6bad70ff7d9
torone89 <torone89>
2022-07-02 00:01:49 +0200

    Esercizio Completo con Fixing del calc del Bonus

--- js/script.js
+++ js/script.js
@@ -2,26 +2,6 @@
 // VERIFICA COLLEGAMENTO JAVA
 console.log("JS")
 
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora 
-// di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
-// # MILESTONE 1
-// Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
-
-// # BONUS
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-
-// #MILESTONE 2
-// Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click
-//  del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
-
-
 // Recupero il bottone
 const btn = document.getElementById('genera');
 console.log("Ho recuperato il " + btn)
@@ -52,27 +32,28 @@ btn.addEventListener('click', () => {
 
     // Variabili per crearmi le celle con il ciclo for
     let riga;
-    let cella;
+    let cell;
 
     switch (selezione) {
         case 'easy': riga = 10
-            cella = 10
+            cell = 10
             // code block
             break;
         case 'medium': riga = 9
-            cella = 9
+            cell = 9
             // code block
             break;
         case 'hard': riga = 7
-            cella = 7
+            cell = 7
             break
         // code block
     }
 
-    const totalecelle = riga * cella
+    const totalecelle = riga * cell
 
     // FUNZIONE
 
+    // Gebertore Celle
     function creatCell(numero, cellaPerRiga) {
         const celle = document.createElement('div');
         celle.className = 'cella';
@@ -82,13 +63,10 @@ btn.addEventListener('click', () => {
         celle.style.height = side;
         celle.style.width = side;
 
-        // #MILESTONE 3
-        // In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
-
         return celle;
     }
     // // FUNZIONE
-    // Genero 17 bombe tramite un ciclo FOR e le inserisco nell'Array Bombs
+    // Genero 16 bombe tramite un ciclo FOR e le inserisco nell'Array Bombs
     // Creo una funzione che generi i numeri che diventeranno le bombe
     function createBomb(min, max) {
 
@@ -112,8 +90,10 @@ btn.addEventListener('click', () => {
     for (let i = 1; i <= totalecelle; i++) {
 
 
-        // Creo cella 
-        const cella = creatCell(i)
+        // Creo cella  (Passo i valori alla funzione  dal ciclo I per numero , 
+        //   e cell Cellperiga della funzione)
+
+        const cella = creatCell(i, cell)
 
         // Appendo la cella alla griglia
         griglia.appendChild(cella)
@@ -121,7 +101,7 @@ btn.addEventListener('click', () => {
         cella.addEventListener('click', function (event) {
 
 
-            //             # MILESTONE 3
+            //             # MILESTONE 1/3
             // Quando l'utente clicca su una cella, verifichiamo se ha calpestato una bomba,
             //  controllando se il numero di cella è presente nell'array di bombe. Se si, la cella diventa rossa 
             // (raccogliamo il punteggio e e scriviamo in console che la partita termina
@@ -142,12 +122,13 @@ btn.addEventListener('click', () => {
                 } else {
                     event.target.classList.add("nobomb");
                     console.log("SALVO,Continua così")
-                    // Aumento di 1 il punteggio utente
+                    // Aumento di 1 il punteggio utente e Calcolo il punteggio
                     risultato++;
                     contatore.innerText = `Punteggio: ${risultato}`;
                     console.log("il tuo risultato è" + risultato)
                 }
 
+                // Decreto la vittoria se finisco le celle o la Sconfitta se piglio la Bomba
                 if (risultato === totalecelle.length - bombe.length) {
                     alert('hai vinto')
                 } else if (event.target.classList.contains("bomb")) { alert("Mi dispiace hai perso") }
